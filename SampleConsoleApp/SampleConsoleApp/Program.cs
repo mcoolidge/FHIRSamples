7d99fdea11c446da131858da77fa3618ec08606d
coolidm <coolidm@ccf.org>
2016-06-28 10:34:19 -0400

    First calls

--- SampleConsoleApp/SampleConsoleApp/Program.cs
+++ SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
 
 namespace SampleConsoleApp
 {
     class Program
     {
+        static string apiKey = "";
+        static string apiURL = "";
+
         static void Main(string[] args)
         {
+            string apiURL = "https://testapi.ccf.org/fhir/";
+
+            var client = new FhirClient(new Uri(apiURL));
+
+            var appointments = FindAppointments(client);
+
+            Console.WriteLine(String.Format("{0} - Results Found", appointments.Total));
+
+            foreach(var appointment in appointments.Entry)
+            {
+                Appointment appt = (Appointment)appointment.Resource;
+                Console.WriteLine(String.Format("{0} - {1}", appt.Start, appt.Description));
+            }
+        }
+
+        static Bundle FindAppointments(FhirClient client)
+        {
+
+            var searchParams = new SearchParams();
+            searchParams.Add("apiKey", apiKey);
+            searchParams.Add("date.after", "6/10/2015");
+            searchParams.Add("date.before", "6/30/2015");
+            searchParams.Add("practitioner.identifier", "1071368");
+            
+            var appointments = client.Search<Appointment>(searchParams);
+
+            return appointments;
+
         }
     }
 }
